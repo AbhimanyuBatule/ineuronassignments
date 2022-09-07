b117c5c7804df014fa92426ad89532ec93f0e4d3
Abhimanyu <abhibatule@gmail.com>
2022-09-07 22:47:48 +0530

    first one-2

--- mongodb1.py
+++ mongodb1.py
@@ -1,6 +1,16 @@
 import pymongo
-
-import pymongo
-client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://Abhimanyu:abhimanyu95@cluster0.ay2s6j1.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
+d = {
+    "name": "abhimanyu",
+    "email":"abhibatule@gmail.com",
+    "subject" : ["maths", "English","Science", "History"]
+}
+database = client['mango1']
+collections = database["Abhi"]
+collections.insert_one(d)
+
+record = collections.find()
+for i in record:
+    print(i)
